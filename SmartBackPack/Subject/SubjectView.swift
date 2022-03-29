c887929fb9a5683df81f114fd25a3f6d8757f4bf
SATHSARA-001 <>
2022-03-29 21:32:40 +0530

    new branch

---
+++ SmartBackPack/Subject/SubjectView.swift
@@ -0,0 +1,20 @@
+//
+//  SubjectView.swift
+//  SmartBackPack
+//
+//  Created by Sathsara Maduranga on 29/3/2022.
+//
+
+import SwiftUI
+
+struct SubjectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SubjectView_Previews: PreviewProvider {
+    static var previews: some View {
+        SubjectView()
+    }
+}
