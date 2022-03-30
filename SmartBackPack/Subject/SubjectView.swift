1758e7ab665fcce19ebcec0a6223e4df48825ee8
Sathsara Maduranga <sathsara.emedia@gmail.com>
2022-03-30 06:00:00 +0530

    add subject ui changes

--- SmartBackPack/Subject/SubjectView.swift
+++ SmartBackPack/Subject/SubjectView.swift
@@ -11,9 +11,9 @@ import Firebase
 struct SubjectView: View {
     
     @StateObject var vm = SubjectVM()
-
+    
     var ref: DatabaseReference! = Database.database().reference()
-
+    
     var body: some View {
         
         ZStack{
@@ -27,28 +27,44 @@ struct SubjectView: View {
                         VStack(alignment: .center, spacing: 20) {
                             
                             Spacer()
-                            Text("Add Books")
-                                .foregroundColor(Color.gray)
-                                .font(.largeTitle)
+                            VStack(alignment: .center, spacing: 5) {
+                                
+                                
+                                Button(action: {
+                                    
+                                    
+                                    
+                                }){
+                                    
+                                    Image("VIP")
+                                        .resizable()
+                                        .frame(width: 200, height: 200)
+                                }
+                        
+                                Text("QR Code Scanned Success")
+                                    .foregroundColor(Color.gray)
+                                    .font(.footnote)
+                            }
                             
                             CustomLabelAndNotEditableTextField(labelTxt:"Name", valueText: $vm.name)
                             CustomLabelAndNotEditableTextField(labelTxt:"QR", valueText: $vm.qrCode)
-                      
-                                Button(action: {
-                                    
-                                    vm.saveUserInDataBase()
-                                 
-                                }){
-                                    
-                                    Text("Save")
-                                        .foregroundColor(Color.white)
-                                        .padding()
-                                        .frame(width: 220, height: 48)
-                                        .background(colorBackground)
-                                        .cornerRadius(24)
-                                }
+                            
+                            Button(action: {
+                                
+                                vm.saveUserInDataBase()
+                                
+                            }){
+                                
+                                Text("Save")
+                                    .foregroundColor(Color.white)
+                                    .padding()
+                                    .frame(width: 220, height: 48)
+                                    .background(colorBackground)
+                                    .cornerRadius(24)
+                            }
                             
                             Spacer()
+                            
                         }
                         .frame(minHeight: geometry.size.height)
                         .padding(.all,20)
